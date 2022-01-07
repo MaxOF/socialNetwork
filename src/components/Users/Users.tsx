665758809de3ad02187d6a1e42050f8b650c834a
Max <egorovteam2019@yandex.ru>
2022-01-07 19:50:17 +0300

    54 - lifecycle methods, componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,13 +6,14 @@ import axios from "axios";
 
 
 class Users extends React.Component<UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props);
+
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
+
     render() {
 
         return (
